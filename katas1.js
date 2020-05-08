7196675980f7dd207d56bdf22aebf7652f6c11a7
davegregg <davegregg@gmail.com>
2020-05-08 14:23:02 -0400

    refactor without functions

--- katas1.js
+++ katas1.js
@@ -1,94 +1,126 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   console.log() the result*/
-    
-}
+/** KATA 01 ⮕
+***
+*** Log the numbers from 1 to 20. (1, 2, 3,..., 19, 20)
+***
+**/
 
-//call function oneThroughTwenty
-
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   console.log() the result */
+  console.log('%cKATA 1 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
   
-}
+  let counter = 1
+  while (counter <= 20) {
+    console.log(counter)
+    counter += 1
+  }
 
-//call function evensToTwenty
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-   
-}
 
-//call function oddsToTwenty
+/** KATA 02 ⮕
+***
+*** Log the even numbers from 1 to 20. (2, 4, 6,...18,20)
+***
+***/
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
+  console.log('%cKATA 02 RESULTS:', 'background: darkblue; color: white; font-weight: bold;') // These console.logs are just to help make the console log better organized and more readable
 
-}
+  /* Write your KATA 02 code in place of THIS comment. */
 
-//call function multiplesOfFive
 
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-    
-}
 
-//call function squareNumbers
+/** KATA 03 ⮕
+***
+*** Log the odd numbers from 1 to 20. (1, 3, 5,...,17,19)
+***
+***/
 
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-}
+  console.log('%cKATA 03 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+ 
+  /* Write your KATA 03 code in place of THIS comment. */
 
-//call function countingBackwards
 
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-}
 
-//call function evenNumbersBackwards
+/** KATA 04 ⮕
+***
+*** Log the multiples of 5 up to 100. (5, 10, 15, ..., 95, 100)
+***
+***/
 
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-}
+  console.log('%cKATA 04 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
-//call function oddNumbersBackwards
+  /* Write your KATA 04 code in place of THIS comment. */
 
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-}
 
-//call function multiplesOfFiveBackwards
 
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  console.log() the result */
-}
+/** KATA 05 ⮕
+***
+*** Log all numbers up to 100 that are perfect squares. (1, 4, 9, ..., 81, 100)
+***
+***/
 
-//call function squareNumbersBackwards
+  console.log('%cKATA 05 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 05 code in place of THIS comment. */
+
+
+
+/** KATA 06 ⮕
+***
+*** Log the numbers counting backwards from 20 to 1. (20, 19, 18, ..., 2, 1)
+***
+***/
+
+  console.log('%cKATA 06 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 06 code in place of THIS comment. */
+
+
+
+/** KATA 07 ⮕
+***
+*** Log the even numbers counting backwards from 20. (20, 18, 16, ..., 4, 2)
+***
+***/
+
+  console.log('%cKATA 07 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 07 code in place of THIS comment. */
+
+
+
+/** KATA 08 ⮕
+***
+*** Log the odd numbers from 20 to 1, counting backwards. (19, 17, 15, ..., 3, 1)
+***
+***/
+
+  console.log('%cKATA 08 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 08 code in place of THIS comment. */
+
+
+
+/** KATA 09 ⮕
+***
+*** Log the multiples of 5, counting down from 100. (100, 95, 90, ..., 10, 5)
+***
+***/
+
+  console.log('%cKATA 09 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 09 code in place of THIS comment. */
+
+
+
+/** KATA 10 ⮕
+***
+*** Log the numbers that are perfect squares, counting down from 100. (100, 81, 64, ..., 4, 1)
+***
+***/
+
+  console.log('%cKATA 10 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+
+  /* Write your KATA 10 code in place of THIS comment. */
+
+
+
+
+  
